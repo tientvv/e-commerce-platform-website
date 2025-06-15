868ae4839c31145b7eaca66a8cbc5d8fe2080c73
tientv <tientranvan19052004@gmail.com>
2025-06-15 13:27:11 +0700

    Project Template

---
+++ e-commerce-user-service-backend/src/main/java/com/tientrannnn/ECommerceUserServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tientrannnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceUserServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceUserServiceBackendApplication.class, args);
+	}
+
+}
