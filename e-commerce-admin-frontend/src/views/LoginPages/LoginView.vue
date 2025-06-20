ddf8543180463ff5249ef02a199301b1d3097221
Tien Tran Van <tientranvan19052004@gmail.com>
2025-06-21 02:57:28 +0700

    feat: restructure project for microservices and add admin/user frontend #2

--- e-commerce-admin-frontend/src/views/LoginPages/LoginView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import LoginForm from './LoginForm.vue'
-</script>
-
-<template>
-  <div class="flex flex-row h-screen justify-center items-center">
-    <LoginForm />
-  </div>
-</template>
