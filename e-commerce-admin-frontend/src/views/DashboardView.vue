8860c5f7926f3860a242a0a3dd21133b06f019d3
Tien Tran Van <tientranvan19052004@gmail.com>
2025-06-21 02:33:26 +0700

    feat: configure Docker Compose for user service backend to enable API development

---
+++ e-commerce-admin-frontend/src/views/DashboardView.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/dashboard">Dashboard</RouterLink>
+    </nav>
+  </div>
+</template>
---
+++ e-commerce-buyer-frontend/src/views/LoginPages/LoginView.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+import LoginForm from './LoginForm.vue'
+import RegisterForm from './RegisterForm.vue'
+
+const activeTab = ref('login')
+
+const switchTab = (tab) => {
+  activeTab.value = tab
+}
+</script>
+
+<template>
+  <div class="h-full text-sm">
+    <div class="flex flex-row items-center h-full">
+      <div class="w-[400px] shadow rounded bg-white">
+        <div class="flex">
+          <button
+            :class="
+              activeTab === 'login'
+                ? 'text-[16px] w-[50%] text-center px-4 py-5 border-b-2 border-blue-500'
+                : 'text-[16px] w-[50%] text-center px-4 py-5 border-b-2 border-gray-300'
+            "
+            @click="switchTab('login')"
+          >
+            Đăng nhập
+          </button>
+          <button
+            :class="
+              activeTab === 'register'
+                ? 'text-[16px] w-[50%] text-center px-4 py-5 border-b-2 border-blue-500'
+                : 'text-[16px] w-[50%] text-center px-4 py-5 border-b-2 border-gray-300'
+            "
+            @click="switchTab('register')"
+          >
+            Đăng ký
+          </button>
+        </div>
+        <div class="p-8">
+          <LoginForm v-if="activeTab === 'login'" />
+          <RegisterForm v-if="activeTab === 'register'" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
