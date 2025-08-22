6f4de7c8a9237a74bf8f610675af2d2ae5f12a62
tientvv <tientranvan19052004@gmail.com>
2025-08-22 18:27:41 +0700

    Delete V3__add_google_id_to_accounts.sql

--- e-commerce-platform-website/back-end/src/main/resources/db/migration/V3__add_google_id_to_accounts.sql
+++
@@ -1,5 +0,0 @@
--- Add google_id column to accounts table if it doesn't exist
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'accounts' AND COLUMN_NAME = 'google_id')
-BEGIN
-    ALTER TABLE accounts ADD google_id VARCHAR(255);
-END
